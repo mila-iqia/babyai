b93350fa62cde03fdf33b6f7dbe74516a9afce2c
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-01-31 17:31:55 -0500

    Updated dependencies

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(
     version='0.0.1',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     install_requires=[
-        'gym>=0.9.0',
+        'gym>=0.9.6',
         'numpy>=1.10.0',
         'pyqt5',
-        'matplotlib',
-        'baselines>=0.1.4'
+        'matplotlib'
     ]
 )
