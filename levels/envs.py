fb9fbae87cf32521462df1b9d8c85435ae1ffab1
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-03-22 14:11:34 -0400

    gen_env: added support for relative locations, objects without color

--- levels/envs.py
+++ levels/envs.py
@@ -1,13 +1,68 @@
 import gym_minigrid
 from gym_minigrid.envs import RoomGrid
+from gym_minigrid.minigrid import COLOR_NAMES
 
 from .instrs import *
 
-# Actions to support:
-# - goto
-# - open
-# - pick
-# - drop
+def door_from_loc(loc):
+    """
+    Get the door index for a given location
+    The door indices correspond to: right, down, left, up
+    """
+
+    if isinstance(loc, AbsLoc):
+        if loc.loc == 'east':
+            return 0
+        if loc.loc == 'south':
+            return 1
+        if loc.loc == 'west':
+            return 2
+        if loc.loc == 'north':
+            return 3
+        assert False, loc
+
+    if isinstance(loc, RelLoc):
+        if loc.loc == 'left':
+            return 3
+        if loc.loc == 'right':
+            return 1
+        if loc.loc == 'front':
+            return 0
+        if loc.loc == 'behind':
+            return 2
+        assert False, loc
+
+    assert False, loc
+
+def room_from_loc(loc):
+    """
+    Get the room coordinates for a given location
+    """
+
+    if isinstance(loc, AbsLoc):
+        if loc.loc == 'north':
+            return (1, 0)
+        if loc.loc == 'south':
+            return (1, 2)
+        if loc.loc == 'west':
+            return (0, 1)
+        if loc.loc == 'east':
+            return (2, 1)
+        assert False, loc
+
+    if isinstance(loc, RelLoc):
+        if loc.loc == 'left':
+            return (1, 0)
+        if loc.loc == 'right':
+            return (1, 2)
+        if loc.loc == 'front':
+            return (2, 1)
+        if loc.loc == 'behind':
+            return (0, 1)
+        assert False, loc
+
+    # By default, use the central room
+    return (1, 1)
 
 def gen_env(instrs, seed):
     """
@@ -21,55 +76,32 @@ def gen_env(instrs, seed):
 
     # For each instruction
     for instr in instrs:
-
         # The pick, goto and open actions mean the referenced objects must exist
         if instr.action == 'pick' or instr.action == 'goto' or instr.action == 'open':
             obj = instr.object
             objs.add(obj)
-
-        # If an object is to be dropped, it doesn't place any constraint on
-        # the environment, except maybe that the location where we drop it must
-        # be reachable
-        elif instr.action == 'drop':
-            pass
-
-        else:
-            assert False, 'unknown action %s' % instr.action
 
     # Create the environment
     # Note: we must have at least 3x3 rooms to support absolute locations
     env = RoomGrid(room_size=7, num_cols=3)
     env.seed(seed)
 
+    # Assign colors to objects that don't have one
+    for obj in objs:
+        if obj.color is None:
+            objs.remove(obj)
+            color = env._randElem(COLOR_NAMES)
+            obj = Object(kind=obj.kind, loc=obj.loc, color=color)
+            objs.add(obj)
+
     # For each object to be added
     for obj in objs:
-
         if obj.kind == 'door':
-            door = 0
-            if isinstance(obj.loc, AbsLoc):
-                if obj.loc.loc == 'east':
-                    door = 0
-                if obj.loc.loc == 'south':
-                    door = 1
-                if obj.loc.loc == 'west':
-                    door = 2
-                if obj.loc.loc == 'north':
-                    door = 3
-
-            env.add_door(1, 1, door, obj.color)
-
-        else:
             room = (1, 1)
-            if isinstance(obj.loc, AbsLoc):
-                if obj.loc.loc == 'north':
-                    room = (1, 0)
-                if obj.loc.loc == 'south':
-                    room = (1, 2)
-                if obj.loc.loc == 'west':
-                    room = (0, 1)
-                if obj.loc.loc == 'east':
-                    room = (2, 1)
-
+            door = door_from_loc(obj.loc)
+            env.add_door(*room, door, obj.color)
+        else:
+            room = room_from_loc(obj.loc)
             env.add_object(*room, obj.kind, obj.color)
 
     # Make sure that all rooms are reachable by the agent
@@ -87,25 +119,31 @@ def test():
     >>> levels.envs.test()
     """
 
-    # TODO: test relative and absolute locations
+    import random
 
-    # TODO: test doors
-
-    # Sample instructions:
-    # [Instr(action="pick", object=Object(color="red", loc=None, state=None, kind="key")),
-    # Instr(action="pick", object=Object(color="red", loc=RelLoc("left"), state=None, kind="key")),
-    # Instr(action="pick", object=Object(color="red", loc=AbsLoc("north"), state=None, kind="key"))]
+    seed = random.randint(0, 0xFFFF)
 
     instrs = [
-        Instr(action="pick", object=Object(color="red", loc=None, kind="key")),
-        Instr(action="drop", object=Object(color="red", loc=None, kind="key")),
-        Instr(action="goto", object=Object(color="blue", loc=AbsLoc('north'), kind="ball")),
-        Instr(action="goto", object=Object(color="green", loc=AbsLoc('east'), kind="box")),
+        Instr(action="pick", object=Object(color="red", loc=RelLoc('front'), kind="key")),
+        #Instr(action="pick", object=Object(color="blue", loc=RelLoc('front'), kind="key")),
 
-        Instr(action="open", object=Object(color="yellow", loc=AbsLoc('north'), kind="door")),
+        Instr(action="pick", object=Object(color=None, loc=RelLoc('left'), kind="ball")),
+
+        #Instr(action="drop", object=Object(color="red", loc=None, kind="key")),
+        #Instr(action="goto", object=Object(color="blue", loc=AbsLoc('north'), kind="ball")),
+        #Instr(action="goto", object=Object(color="green", loc=AbsLoc('east'), kind="box")),
+        #Instr(action="open", object=Object(color="yellow", loc=AbsLoc('north'), kind="door")),
     ]
 
-    env = gen_env(instrs, 0)
+    print(instrs)
+
+    env = gen_env(instrs, seed)
+
+    def keyDownCb(keyName):
+        if keyName == 'ESCAPE':
+            env.gridRender.window.close()
+    renderer = env.render('human')
+    renderer.window.setKeyDownCb(keyDownCb)
 
     while True:
         env.render('human')
--- levels/instrs.py
+++ levels/instrs.py
@@ -1,7 +1,16 @@
 from collections import namedtuple
 
+# Note: we may want to have an implicit parameter to distinguish between
+# locations that are near or far. This way, the difficulty level would be
+# implicitly represented in instructions.
+
+# Actions: goto, open, pick, drop
 Instr = namedtuple('Instr', ['action', 'object'])
+
 Object = namedtuple('Object', ['kind', 'color', 'loc'])
 
-RecLoc = namedtuple('RelLoc', ['loc'])
+# Relative locations: left, right, front, behind
+RelLoc = namedtuple('RelLoc', ['loc'])
+
+# Absolute locations: north, south, east, west
 AbsLoc = namedtuple('RelLoc', ['loc'])
