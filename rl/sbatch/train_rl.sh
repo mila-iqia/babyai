810a670b5b043c6f8e7ad79536c6c278c6fa3dc6
saleml <salemlahlou9@gmail.com>
2018-05-19 22:47:16 -0400

    removed unnecessary files

--- rl/sbatch/train_rl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-
-# Load appropriate modules
-source activate deep
-
-# Execute Python application in unbuffered mode.
-export PYTHONUNBUFFERED=1
-exec $@
