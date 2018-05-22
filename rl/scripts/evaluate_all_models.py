aaf250288fa3c84b103c2dfdcefa578fa8211c50
Lucas Willems <lcswillems@gmail.com>
2018-05-22 17:00:33 -0400

    Added `evaluate_all_demos` and updated `evaluate_all_models`

--- rl/scripts/evaluate_all_models.py
+++ rl/scripts/evaluate_all_models.py
@@ -4,8 +4,9 @@ import sys
 
 import utils
 
-models_folder = os.path.join(utils.storage_dir(), "models")
-for model in os.listdir(models_folder):
+folder = os.path.join(utils.storage_dir(), "models")
+for model in sorted(os.listdir(folder)):
     env = "BabyAI-{}-v0".format(model.split("_")[0])
     print("> Env: {}".format(env))
-    call(["python -m scripts.evaluate --env {} --model {}".format(env, model)] + sys.argv[1:], shell=True)
+    command = ["python -m scripts.evaluate --env {} --model {}".format(env, model)] + sys.argv[1:]
+    call(" ".join(command), shell=True)
