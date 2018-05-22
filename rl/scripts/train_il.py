feb41fa5eb8ad69c2f1cecb5b83c40773bbc6ff5
Lucas Willems <lcswillems@gmail.com>
2018-05-22 09:47:54 -0400

    Updated `train_il` to new code. Not working with recurrent policy

--- rl/scripts/train_il.py
+++ rl/scripts/train_il.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-# TODO the whole file has to be changed - it's an old version right now
+# TODO add support for recurrent policy
 
 import argparse
 import gym
-import gym_minigrid
 import levels
 import time
 import datetime
-import numpy as np
+import numpy
 import sys
-import random
 import torch
-from torch.autograd import Variable
 import torch.nn.functional as F
-
 import torch_rl
+
 import utils
 
 # Parse arguments
@@ -22,46 +19,60 @@ import utils
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", required=True,
                     help="name of the environment to train on (REQUIRED)")
+parser.add_argument("--demos-origin", default=None,
+                    help="origin of the demonstrations: human | agent (REQUIRED)")
 parser.add_argument("--model", default=None,
-                    help="name of the pre-trained model (default: ENV_il)")
+                    help="name of the model (default: ENV_ORIGIN_il)")
 parser.add_argument("--seed", type=int, default=1,
                     help="random seed (default: 1)")
-parser.add_argument("--demonstrations", type=int, default=100,
-                    help="number of demonstrations to use (default: 100)")
+parser.add_argument("--episodes", type=int, default=100,
+                    help="number of episodes of demonstrations to use (default: 100)")
 parser.add_argument("--log-interval", type=int, default=1,
-                    help="interval between log display (default: 1)")
+                    help="number of updates between two logs (default: 1)")
 parser.add_argument("--save-interval", type=int, default=0,
-                    help="interval between model sactionaving (default: 0, 0 means no saving)")
+                    help="number of updates between two saves (default: 0, 0 means no saving)")
+parser.add_argument("--tb", action="store_true", default=False,
+                    help="log into Tensorboard")
 parser.add_argument("--lr", type=float, default=7e-4,
                     help="learning rate (default: 7e-4)")
 parser.add_argument("--entropy-coef", type=float, default=0.01,
                     help="entropy term coefficient (default: 0.01)")
+parser.add_argument("--recurrence", type=int, default=1,
+                    help="number of timesteps gradient is backpropagated (default: 1)")
 parser.add_argument("--optim-eps", type=float, default=1e-5,
                     help="Adam optimizer epsilon (default: 1e-5)")
 parser.add_argument("--epochs", type=int, default=10,
                     help="number of epochs (default: 10)")
 parser.add_argument("--batch-size", type=int, default=256,
-                    help="batch size (default: 256, 0 means all)")
+                    help="batch size (default: 256)")
+parser.add_argument("--model-instr", action="store_true", default=False,
+                    help="use instructions in the model")
+parser.add_argument("--model-mem", action="store_true", default=False,
+                    help="use memory in the model")
+parser.add_argument("--arch", default='cnn1',
+                    help="image embedding architecture")
 args = parser.parse_args()
 
-# Set numpy and pytorch seeds
+# Set seed for all randomness sources
 
-torch_ac.seed(args.seed)
+utils.seed(args.seed)
 
-# Generate environments
+# Generate environment
 
 env = gym.make(args.env)
 
 # Load demonstrations
 
-demos = utils.load_demos(args.env)[:args.demonstrations]
+demos = utils.load_demos(args.env, args.demos_origin)[:args.episodes]
 flat_demos = []
 for demo in demos:
     flat_demos += demo
 
 # Define model name
 
-model_name = args.model or args.env + "_" + "il"
+default_model_name = "{}_{}_il".format(args.env, args.demos_origin)
+model_name = args.model or default_model_name
+print("The model is saved in {}".format(model_name))
 
 # Define obss preprocessor
 
@@ -70,23 +81,39 @@ obss_preprocessor = utils.ObssPreprocessor(model_name, env.observation_space)
 # Define actor-critic model
 
 acmodel = utils.load_model(obss_preprocessor.obs_space, env.action_space, model_name)
-if torch_ac.gpu_available:
+if torch.cuda.is_available():
     acmodel.cuda()
 
-# Define optimizer
+# Define optimizer and loss
 
 optimizer = torch.optim.Adam(acmodel.parameters(), args.lr, eps=args.optim_eps)
-
-# Define loss
-
 loss = torch.nn.CrossEntropyLoss()
 
-# Define logger and log command and model
+# Define logger and Tensorboard writer
 
-suffix = datetime.datetime.now().strftime("%y%m%d%H%M%S")
-logger = utils.Logger(model_name + "_" + suffix)
-logger.log(" ".join(sys.argv), to_print=False)
+logger = utils.Logger(model_name)
+if args.tb:
+    from tensorboardX import SummaryWriter
+    writer = SummaryWriter(utils.get_log_path(model_name, ext=False))
+
+# Log command, availability of CUDA, and model
+
+logger.log(args, to_print=False)
+logger.log("CUDA is {}available".format('' if torch.cuda.is_available() else 'not '))
 logger.log(acmodel)
+
+# Prepare experiences
+
+obs, action, reward, done = zip(*flat_demos)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+exps = torch_rl.DictList({
+    "obs": obss_preprocessor(obs, device=device),
+    "action": torch.tensor(action, device=device),
+    "reward": torch.tensor(reward, device=device),
+    "mask": 1 - torch.tensor(done, device=device, dtype=torch.float)
+})
 
 # Train model
 
@@ -97,32 +124,25 @@ batch_size = len(flat_demos) if args.batch_size == 0 else args.batch_size
 for i in range(1, args.epochs + 1):
     # Update parameters
 
-    random.shuffle(flat_demos)
-
-    obs, action = zip(*flat_demos)
-    action = torch.from_numpy(np.array(action)).unsqueeze(1)
-    if torch_ac.gpu_available:
-        action = action.cuda()
+    numpy.random.shuffle(flat_demos)
 
     log_entropies = []
-    log_action_losses = []
+    log_policy_losses = []
+
+    update_start_time = time.time()
 
     for j in range(0, len(flat_demos), batch_size):
-        b_obs = obs[j:j + batch_size]
-        b_action = action[j:j + batch_size]
+        b = exps[j:j + batch_size]
 
         # Compute loss
 
-        preprocessed_obs = obss_preprocessor(b_obs, volatile=False, use_gpu=torch_ac.gpu_available)
-        rdist = acmodel.get_rdist(preprocessed_obs)
+        dist, _ = acmodel(b.obs)
 
-        log_dist = F.log_softmax(rdist, dim=1)
-        dist = F.softmax(rdist, dim=1)
-        entropy = -(log_dist * dist).sum(dim=1).mean()
+        entropy = dist.entropy().mean()
 
-        action_loss = -log_dist.gather(1, Variable(b_action)).mean()
+        policy_loss = -dist.log_prob(b.action).mean()
 
-        loss = action_loss - args.entropy_coef * entropy
+        loss = policy_loss - args.entropy_coef * entropy
 
         # Update actor-critic
 
@@ -133,23 +153,38 @@ for i in range(1, args.epochs + 1):
         # Log some values
 
         log_entropies.append(entropy.data[0])
-        log_action_losses.append(action_loss.data[0])
+        log_policy_losses.append(policy_loss.data[0])
+    
+    update_end_time = time.time()
 
     # Print logs
 
     if i % args.log_interval == 0:
         total_ellapsed_time = int(time.time() - total_start_time)
+        fps = len(flat_demos)/(update_end_time - update_start_time)
+        duration = datetime.timedelta(seconds=total_ellapsed_time)
+
+        log_entropy = numpy.mean(log_entropies)
+        log_policy_loss = numpy.mean(log_policy_losses)
 
         logger.log(
-            "U {} | D {} | H {:.3f} | aL {: .3f}"
-                .format(i, datetime.timedelta(seconds=total_ellapsed_time),
-                        np.mean(log_entropies), np.mean(log_action_losses)))
+            "U {} | FPS {:04.0f} | D {} | H {:.3f} | pL {: .3f}"
+                .format(i, fps, duration,
+                        log_entropy, log_policy_loss))
+    
+        if args.tb:
+            writer.add_scalar("FPS", fps, i)
+            writer.add_scalar("duration", total_ellapsed_time, i)
+            writer.add_scalar("entropy", log_entropy, i)
+            writer.add_scalar("policy_loss", log_policy_loss, i)
 
-    # Save model
+    # Save obss preprocessor vocabulary and model
 
     if args.save_interval > 0 and i % args.save_interval == 0:
-        if torch_ac.gpu_available:
+        obss_preprocessor.vocab.save()
+
+        if torch.cuda.is_available():
             acmodel.cpu()
         utils.save_model(acmodel, model_name)
-        if torch_ac.gpu_available:
+        if torch.cuda.is_available():
             acmodel.cuda()
