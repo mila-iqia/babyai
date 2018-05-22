24454977e2d67f32d0d7cf3aa8ded0553814e17c
Lucas Willems <lcswillems@gmail.com>
2018-05-22 11:38:28 -0400

    Removed useless print in `synthesize_demos`

--- rl/utils/demos.py
+++ rl/utils/demos.py
@@ -19,10 +19,6 @@ def save_demos(demos, env_name, origin):
 
 def synthesize_demos(demos):
     print('{} demonstrations saved'.format(len(demos)))
-    reward_per_episode = [sum([step[2] for step in demo]) for demo in demos]
     num_frames_per_episode = [len(demo) for demo in demos]
     if len(demos) > 0:
-        print('Demo steps: {}'.format(num_frames_per_episode))
-        print('R:x̄σmM {: .3f} {: .3f} {: .3f} {: .3f} | F:x̄σmM {:.1f} {:.1f} {} {}'
-              .format(*utils.synthesize(reward_per_episode).values(),
-                      *utils.synthesize(num_frames_per_episode).values()))
+        print('Demo num frames: {}'.format(num_frames_per_episode))
