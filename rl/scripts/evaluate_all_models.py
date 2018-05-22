26b5b6842710709e41e3f957b38fd9769de17fd7
saleml <salemlahlou9@gmail.com>
2018-05-22 17:40:59 -0400

    small fix of "evaluate_all_models" to skip hidden files/folders

--- rl/scripts/evaluate_all_models.py
+++ rl/scripts/evaluate_all_models.py
@@ -6,6 +6,8 @@ import utils
 
 folder = os.path.join(utils.storage_dir(), "models")
 for model in sorted(os.listdir(folder)):
+    if model.startswith('.'):
+        continue
     env = "BabyAI-{}-v0".format(model.split("_")[0])
     print("> Env: {}".format(env))
     command = ["python -m scripts.evaluate --env {} --model {}".format(env, model)] + sys.argv[1:]
