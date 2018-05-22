b1f9248b9208a6a71f8a77af963fcf93005190a6
saleml <salemlahlou9@gmail.com>
2018-05-22 10:59:27 -0400

    added synthesis of rewards when making demos

--- rl/utils/demos.py
+++ rl/utils/demos.py
@@ -19,7 +19,11 @@ def save_demos(demos, env_name, origin):
 
 def synthesize_demos(demos):
     print('{} demonstrations saved'.format(len(demos)))
+    rewards = [[step[2] for step in demo] for demo in demos]
+    assert all([sum(reward) == reward[-1] for reward in rewards])
+    reward_per_episode = [sum(reward) for reward in rewards]
     demo_lens = [len(demo) for demo in demos]
     if len(demos) > 0:
         print('Demo sizes: {}'.format(demo_lens))
-        print('Synthesis: {}'.format(utils.synthesize(demo_lens)))
+        print('Synthesis (steps - reward): {} - {}'.format(utils.synthesize(demo_lens),
+                                                           utils.synthesize(reward_per_episode)))
