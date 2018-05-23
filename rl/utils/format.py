2317ac78b92ac93cba207cdb409348f211f0e47d
saleml <salemlahlou9@gmail.com>
2018-05-23 13:40:20 -0400

    Remove the normalization option as it turns out it is useless

--- rl/utils/format.py
+++ rl/utils/format.py
@@ -30,8 +30,7 @@ class Vocabulary:
         json.dump(self.vocab, open(self.path, "w"))
 
 class ObssPreprocessor:
-    def __init__(self, model_name, obs_space, normalize=False):
-        self.normalize = normalize
+    def __init__(self, model_name, obs_space):
         self.vocab = Vocabulary(model_name)
         self.obs_space = {
             "image": 147,
@@ -43,8 +42,6 @@ class ObssPreprocessor:
 
         if "image" in self.obs_space.keys():
             images = numpy.array([obs["image"] for obs in obss])
-            if self.normalize:
-                images /= 10
             images = torch.tensor(images, device=device, dtype=torch.float)
 
             obs_.image = images
