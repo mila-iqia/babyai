2a9b77eec17a7e02bcdcb48ca6103deac56b1d67
Chitwan <chitwan@Chitwans-MacBook-Pro.local>
2018-05-28 15:07:12 -0400

    Modifications to train_wd

--- rl/scripts/train_wd.py
+++ rl/scripts/train_wd.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# TODO add support for recurrent policy
 
 import argparse
 import gym
@@ -59,6 +58,8 @@ parser.add_argument("--value-loss-coef", type=float, default=0.5,
                     help="value loss term coefficient (default: 0.5)")
 
 
+
+
 args = parser.parse_args()
 
 
@@ -70,160 +71,258 @@ env = gym.make(args.env)
 
 
 demos = utils.load_demos(args.env, args.demos_origin)[:args.episodes]
+print(len(demos))
+demos.sort(key=len,reverse=True)
 
-obss_preprocessor = utils.ObssPreprocessor(args.model, env.observation_space)
+
+
+default_model_name = "{}_{}_il".format(args.env, args.demos_origin)
+model_name = args.model or default_model_name
+print("The model is saved in {}".format(model_name))
+
+obss_preprocessor = utils.ObssPreprocessor(model_name, env.observation_space)
+
+# Define logger and Tensorboard writer
+
+logger = utils.Logger(model_name)
+if args.tb:
+    from tensorboardX import SummaryWriter
+    writer = SummaryWriter(utils.get_log_path(model_name, ext=False))
 
 
 # Define actor-critic model
 
-acmodel = utils.load_model(obss_preprocessor.obs_space, env.action_space, args.model,
+acmodel = utils.load_model(obss_preprocessor.obs_space, env.action_space, model_name,
                             args.model_instr, args.model_mem, args.arch)
+
+acmodel.train()
+
+
+# Log command, availability of CUDA, and model
+
+logger.log(args, to_print=False)
+logger.log("CUDA is {}available".format('' if torch.cuda.is_available() else 'not '))
+logger.log(acmodel)
 
 
 if torch.cuda.is_available():
     acmodel.cuda()
 
 optimizer = torch.optim.Adam(acmodel.parameters(), args.lr, eps=args.optim_eps)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.9)
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# flat_demos = []
-# for demo in demos:
-#     flat_demos += demo
-
-def starting_indexes(max_len):
-    return np.arange(0, max_len*args.batch_size, args.recurrence)
 
 
-def collect_experiences():
+def calculate_values():
+    flat_demos = []
+    value_inds = [0]
+
+    for demo in demos:
+        flat_demos += demo
+        value_inds.append(value_inds[-1] + len(demo))
+    flat_demos = np.array(flat_demos)
+    value_inds = value_inds[:-1]
+    value_inds = [index-1 for index in value_inds][1:] +[len(flat_demos)-1]
+
+    values = np.zeros([len(flat_demos)],dtype=np.float64)
+
+    reward, done = flat_demos[:,2], flat_demos[:,3]
+
+    reward = utils.reshape_reward(None,None,reward,None)
+
+    values[value_inds]= reward[value_inds]
+    last_value = values[value_inds]
+    
+    while True:
+        value_inds = [index-1 for index in value_inds]
+        if value_inds[0] == -1:
+            break
+        done_step = done[value_inds]
+        value_inds = value_inds[:len(value_inds)-sum(done_step)]
+        last_value = last_value[:len(last_value)-sum(done_step)]
+       
+        values[value_inds]= reward[value_inds] + args.discount*last_value
+        last_value = values[value_inds]
+    
+    flat_demos = [np.append(flat_demos[i],[values[i],]) for i in range(len(flat_demos))]
+    new_demos = []
+    offset = 0
+
+    for demo in demos:
+        new_demos.append(flat_demos[offset:offset+len(demo)])
+        offset += len(demo)
+
+    flat_demos = np.array(flat_demos)
+    return new_demos,flat_demos
+
+def run_epoch_norecur():
+    np.random.shuffle(flat_demos)
+    batch_size = args.batch_size
+
+    log = {"entropy": [],"value_loss": [],"policy_loss": []}
+
+    for j in range(0, len(flat_demos), batch_size):
+        flat_batch = flat_demos[j:j + batch_size,:]
+        obs, action_true, values, done = flat_batch[:,0], flat_batch[:,1], flat_batch[:,4], flat_batch[:,3]
+        preprocessed_obs = obss_preprocessor(obs, device=device)
+
+        action_true = torch.tensor([action for action in action_true],device=device,dtype=torch.float)
+        values = torch.tensor([value for value in values],device=device,dtype=torch.float)
+        memory = torch.zeros([batch_size,acmodel.memory_size], device=device)
+        # Compute loss
+
+        dist, value, memory = acmodel(preprocessed_obs,memory)
+
+        entropy = dist.entropy().mean()
+
+        policy_loss = -dist.log_prob(action_true).mean()
+
+        value_loss = (value - values).pow(2).mean()
+        loss = policy_loss - args.entropy_coef * entropy + args.value_loss_coef * value_loss
+
+        # Update actor-critic
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        log["entropy"].append(float(entropy))
+        log["policy_loss"].append(float(policy_loss))
+        log["value_loss"].append(float(value_loss))
+
+    return log
+    
+
+
+def run_epoch_recurrence():
+    np.random.shuffle(demos)
+    
     batch_size = args.batch_size
     offset = 0
     assert len(demos) % batch_size == 0
-    # demos.sort(key=len)
+    log = {"entropy": [],"value_loss": [],"policy_loss": []}
 
     for batch_index in range(len(demos)//batch_size):
         batch = demos[offset:offset+batch_size]
-        max_len = max(len(item) for item in batch)
-        obss = []
+        batch.sort(key=len,reverse=True)
 
-        reward_mat = np.zeros((batch_size,max_len),dtype=np.float64)
-        action_true = np.zeros((batch_size,max_len),dtype=np.float64)
-        mask_mat = np.zeros((batch_size,max_len),dtype = np.float64)
-        for i in range(batch_size):
-            temp = []
-            for index in range(max_len):
-                j = min(index,len(batch[i])-1)
-                temp.append(batch[i][j][0])
-                reward_mat[i,index] = (index<len(batch[i]))*batch[i][j][2]
-                action_true[i,index] = (index<len(batch[i]))*batch[i][j][1]
-                mask_mat[i,index] = (index<len(batch[i]))
-            obss.append(temp)
-
-        reward_mat = utils.reshape_reward(None,None,reward_mat,None)
-
-        obss = np.stack(obss,axis=0)
-
-        if args.model_mem:
-            memories = torch.zeros([batch_size,max_len,acmodel.memory_size], device=device)
-            memory = torch.zeros([batch_size,acmodel.memory_size], device=device)
+        flat_batch = []
+        inds = [0]
+        for demo in batch:
+            flat_batch += demo
+            inds.append(inds[-1] + len(demo))
+        flat_batch = np.array(flat_batch)
+        inds = inds[:-1]
 
 
-        for i in range(max_len):
-            preprocessed_obs = obss_preprocessor(obss[:,i], device=device)
+        obss, action_true, values, done = flat_batch[:,0], flat_batch[:,1], flat_batch[:,4], flat_batch[:,3]
+        action_true = torch.tensor([action for action in action_true],device=device,dtype=torch.float)        
+        values = torch.tensor([value for value in values],device=device,dtype=torch.float)
+
+        
+        memories = torch.zeros([len(flat_batch),acmodel.memory_size], device=device)
+        memory = torch.zeros([batch_size,acmodel.memory_size], device=device)
+
+        time_step = 0
+        time_step_inds = inds
+        
+        while True:
+            obs = obss[time_step_inds]
+            done_step = done[time_step_inds]
+            preprocessed_obs = obss_preprocessor(obs, device=device)
             with torch.no_grad():
                 if args.model_mem:
-                    dist, _, new_memory = acmodel(preprocessed_obs, memory)
-                else:
-                    dist, _ = acmodel(preprocessed_obs)        
+                    _, _, new_memory = acmodel(preprocessed_obs, memory[:len(time_step_inds),:])    
 
             if args.model_mem:
-                memories[:,i] = memory
-                memory = new_memory
-        
-        values = np.zeros((batch_size,max_len),dtype=np.float64)
-        values[:,max_len-1] = reward_mat[:,max_len-1]
-
-        for i in reversed(range(max_len-1)):
-            values[:,i] = reward_mat[:,i] + args.discount*values[:,i+1]
-
-        action_true = torch.tensor(action_true,dtype=torch.float,device=device)
-        reward_mat = torch.tensor(reward_mat,dtype=torch.float,device=device)
-        values = torch.tensor(values,dtype=torch.float,device=device)
-        mask_mat = torch.tensor(mask_mat,dtype=torch.float,device=device)
-
-        exps = DictList()
-        exps.obs = [obs for temp in obss for obs in temp]
-        if args.model_mem:
-            exps.memory = memories.view(-1, *memories.shape[2:])
-            exps.mask = mask_mat.view(-1, *mask_mat.shape[2:])
-        exps.action_true = action_true.view(-1, *action_true.shape[2:])
-        exps.returnn = values.view(-1, *values.shape[2:])
+                for i in range(len(time_step_inds)):
+                    memories[time_step_inds[i],:] = memory[i,:]
+                memory[:len(time_step_inds),:] = new_memory
+            time_step_inds = time_step_inds[:len(time_step_inds)-sum(done_step)]
+            if len(time_step_inds) == 0:
+                break
+            time_step_inds = [index+1 for index in time_step_inds]
 
 
-        exps.obs = obss_preprocessor(exps.obs, device=device)
-        inds = starting_indexes(max_len)
-        
-        if args.model_mem:
-            memory = exps.memory[inds]
+        while True:
+            memory = memories[inds,:]
+            final_loss = 0
+            for i in range(args.recurrence):
+                obs = obss[inds]
+                preprocessed_obs = obss_preprocessor(obs, device=device)
+                action_step = action_true[inds]
+                done_step = done[inds]
+                if args.model_mem:
+                    dist, value, memory = acmodel(preprocessed_obs, memory)
 
-        update_entropy = 0
-        update_policy_loss = 0
-        update_value_loss = 0
-        update_loss = 0
+                entropy = dist.entropy().mean()
+                policy_loss = -dist.log_prob(action_step).mean()
+                value_loss = (value - values[inds]).pow(2).mean()
+                loss = policy_loss - args.entropy_coef * entropy + args.value_loss_coef * value_loss
+                final_loss += loss
 
-        for _ in range(args.recurrence):
-            # Create a sub-batch of experience
-            sb = exps[inds]
+                # Log some values
+                log["entropy"].append(float(entropy))
+                log["policy_loss"].append(float(policy_loss))
+                log["value_loss"].append(float(value_loss))
 
-            # Compute loss
+                inds = inds[:len(inds)-sum(done_step)]
+                memory = memory[:len(inds)]
+                if len(inds) == 0:
+                    break
+                inds = [index+1 for index in inds]
 
-            if args.model_mem:
-                dist, value, memory = acmodel(sb.obs, memory)
-            else:
-                dist, value = acmodel(sb.obs)
+            final_loss /= args.recurrence
+            optimizer.zero_grad()
+            final_loss.backward()
+            optimizer.step()
 
-            entropy = (dist.entropy()*sb.mask).mean()
-
-
-            policy_loss = -(dist.log_prob(sb.action_true)*sb.mask).mean()
-
-            value_loss = ((value - sb.returnn)*sb.mask).pow(2).mean()
-            
-            loss = policy_loss - args.entropy_coef * entropy + args.value_loss_coef * value_loss
-
-            # Update batch values
-
-            update_entropy += entropy.item()
-            update_policy_loss += policy_loss.item()
-            update_value_loss += value_loss.item()
-            update_loss += loss
-
-            # Update indexes
-
-            inds += 1
-
-        optimizer.zero_grad()
-        update_loss.backward()
-        optimizer.step()
-
-        print("Batch %d : " % (batch_index+1),end="\t")
-        print("Entropy %.3f" % update_entropy,end="\t")
-        print("Value Loss %.3f" % update_value_loss,end="\t")
-        print("Policy Loss %.3f" % update_policy_loss,end="\t")
-        print("Total Loss %.3f" % update_loss)
-
+            if len(inds) == 0:
+                break     
 
         offset += batch_size
+        return log
 
+total_start_time = time.time()
 
-for i in range(args.epochs):
-    print("Epoch %d" % (i+1))
-    collect_experiences()
+demos,flat_demos = calculate_values()
+
+for i in range(1,args.epochs+1): 
+    update_start_time = time.time()   
+    scheduler.step()
+    
+    if args.model_mem:
+        log = run_epoch_recurrence()
+    else:
+        log = run_epoch_norecur()
+
+    update_end_time = time.time()
+
+    # Print logs
+    total_ellapsed_time = int(time.time() - total_start_time)
+    fps = len(flat_demos)/(update_end_time - update_start_time)
+    duration = datetime.timedelta(seconds=total_ellapsed_time)
+
+    for key in log:
+        log[key] = np.mean(log[key])
+
+    logger.log(
+        "U {} | FPS {:04.0f} | D {} | H {:.3f} | pL {: .3f} | vL {: .3f}"
+            .format(i, fps, duration,
+                    log["entropy"], log["policy_loss"],log["value_loss"]))
+
+    if args.tb:
+        writer.add_scalar("FPS", fps, i)
+        writer.add_scalar("duration", total_ellapsed_time, i)
+        writer.add_scalar("entropy", log["entropy"], i)
+        writer.add_scalar("policy_loss", log["policy_loss"], i)
 
     if args.save_interval > 0 and i % args.save_interval == 0:
         obss_preprocessor.vocab.save()
         if torch.cuda.is_available():
             acmodel.cpu()
-        utils.save_model(acmodel, args.model)
+        utils.save_model(acmodel, model_name)
         if torch.cuda.is_available():
             acmodel.cuda()
 
