461465a70ed56428b58bc1e812e5eedc638fe2e5
Chitwan <chitwan@Chitwans-MBP.local>
2018-05-31 10:43:25 -0400

    added early stopping

--- rl/scripts/train_wd.py
+++ rl/scripts/train_wd.py
@@ -16,7 +16,6 @@ from torch_rl.utils import DictList
 import utils
 
 # Parse arguments
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", required=True,
                     help="name of the environment to train on (REQUIRED)")
@@ -24,14 +23,14 @@ parser.add_argument("--demos-origin", default=None,
                     help="origin of the demonstrations: human | agent (REQUIRED)")
 parser.add_argument("--model", default=None,
                     help="name of the model (default: ENV_ORIGIN_il)")
-parser.add_argument("--seed", type=int, default=1,
-                    help="random seed (default: 1)")
+parser.add_argument("--seed", type=int, default=0,
+                    help="random seed (default: 0)")
 parser.add_argument("--episodes", type=int, default=100,
                     help="number of episodes of demonstrations to use (default: 100)")
 parser.add_argument("--log-interval", type=int, default=1,
                     help="number of updates between two logs (default: 1)")
-parser.add_argument("--save-interval", type=int, default=0,
-                    help="number of updates between two saves (default: 0, 0 means no saving)")
+parser.add_argument("--save-interval", type=int, default=10,
+                    help="number of updates between two saves (default: 10, 0 means no saving)")
 parser.add_argument("--tb", action="store_true", default=False,
                     help="log into Tensorboard")
 parser.add_argument("--lr", type=float, default=7e-4,
@@ -56,25 +55,22 @@ parser.add_argument("--discount", type=float, default=0.99,
                     help="discount factor (default: 0.99)")
 parser.add_argument("--value-loss-coef", type=float, default=0.5,
                     help="value loss term coefficient (default: 0.5)")
-
-
+parser.add_argument("--validation-interval", type=int, default=10,
+                    help="number of epochs between two validation checks (default: 10)")
+parser.add_argument("--val-episodes", type=int, default=100,
+                    help="number of episodes used for validation (default: 100)")
+parser.add_argument("--patience", type=int, default=3,
+                    help="patience for early stopping (default: 3)")
+parser.add_argument("--val-seed", type=int, default=0,
+                    help="seed for environment used for validation (default: 0)")
 
 
 args = parser.parse_args()
-
-
 utils.seed(args.seed)
-
-# Generate environment
-
 env = gym.make(args.env)
 
-
 demos = utils.load_demos(args.env, args.demos_origin)[:args.episodes]
-print(len(demos))
 demos.sort(key=len,reverse=True)
-
-
 
 default_model_name = "{}_{}_il".format(args.env, args.demos_origin)
 model_name = args.model or default_model_name
@@ -82,8 +78,9 @@ print("The model is saved in {}".format(model_name))
 
 obss_preprocessor = utils.ObssPreprocessor(model_name, env.observation_space)
 
-# Define logger and Tensorboard writer
 
+
+# Define logger and Tensorboard writer
 logger = utils.Logger(model_name)
 if args.tb:
     from tensorboardX import SummaryWriter
@@ -91,7 +88,6 @@ if args.tb:
 
 
 # Define actor-critic model
-
 acmodel = utils.load_model(obss_preprocessor.obs_space, env.action_space, model_name,
                             args.model_instr, args.model_mem, args.arch)
 
@@ -99,11 +95,9 @@ acmodel.train()
 
 
 # Log command, availability of CUDA, and model
-
 logger.log(args, to_print=False)
 logger.log("CUDA is {}available".format('' if torch.cuda.is_available() else 'not '))
 logger.log(acmodel)
-
 
 if torch.cuda.is_available():
     acmodel.cuda()
@@ -111,9 +105,7 @@ if torch.cuda.is_available():
 optimizer = torch.optim.Adam(acmodel.parameters(), args.lr, eps=args.optim_eps)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.9)
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 def calculate_values():
     flat_demos = []
@@ -173,17 +165,17 @@ def run_epoch_norecur():
     batch_size = args.batch_size
 
     log = {"entropy": [],"value_loss": [],"policy_loss": []}
-
-    for j in range(0, len(flat_demos), batch_size):
-        flat_batch = flat_demos[j:j + batch_size,:]
+    offset = 0
+    for j in range(len(flat_demos)//batch_size):
+        flat_batch = flat_demos[offset:offset+batch_size,:]
         obs, action_true, values, done = flat_batch[:,0], flat_batch[:,1], flat_batch[:,4], flat_batch[:,3]
         preprocessed_obs = obss_preprocessor(obs, device=device)
 
         action_true = torch.tensor([action for action in action_true],device=device,dtype=torch.float)
         values = torch.tensor([value for value in values],device=device,dtype=torch.float)
         memory = torch.zeros([batch_size,acmodel.memory_size], device=device)
+        
         # Compute loss
-
         dist, value, memory = acmodel(preprocessed_obs,memory)
 
         entropy = dist.entropy().mean()
@@ -201,10 +193,15 @@ def run_epoch_norecur():
         log["entropy"].append(float(entropy))
         log["policy_loss"].append(float(policy_loss))
         log["value_loss"].append(float(value_loss))
+        offset += batch_size
 
     return log
     
-
+def starting_indexes(num_frames):
+    if num_frames % args.recurrence == 0:
+        return np.arange(0, num_frames, args.recurrence)
+    else:
+        return np.arange(0, num_frames, args.recurrence)[:-1]
 
 def run_epoch_recurrence():
     np.random.shuffle(demos)
@@ -220,14 +217,21 @@ def run_epoch_recurrence():
         batch = demos[offset:offset+batch_size]
         batch.sort(key=len,reverse=True)
 
-        # Constructing flat batch and indices pointing to start of each demonstration 
+        # Constructing flat batch and indices pointing to start of each demonstration
         flat_batch = []
         inds = [0]
+        
         for demo in batch:
             flat_batch += demo
             inds.append(inds[-1] + len(demo))
+        
         flat_batch = np.array(flat_batch)
         inds = inds[:-1]
+        num_frames = len(flat_batch)
+        
+        mask = np.ones([len(flat_batch)],dtype=np.float64)
+        mask[inds] = 0
+        mask = torch.tensor(mask,device=device,dtype=torch.float).unsqueeze(1)
 
         # Observations, true action, values and done for each of the stored demostration
         obss, action_true, values, done = flat_batch[:,0], flat_batch[:,1], flat_batch[:,4], flat_batch[:,3]
@@ -238,88 +242,131 @@ def run_epoch_recurrence():
         memories = torch.zeros([len(flat_batch),acmodel.memory_size], device=device)
         memory = torch.zeros([batch_size,acmodel.memory_size], device=device)
 
-        # time_step_inds to be used for calculating the memory for each observation in flat_batch
-        time_step = 0
-        time_step_inds = inds
-        
         # Loop terminates when every observation in the flat_batch has been handled
         while True:
-            # taking observations and done located at time_step_inds
-            obs = obss[time_step_inds]
-            done_step = done[time_step_inds]
+            # taking observations and done located at inds
+            obs = obss[inds]
+            done_step = done[inds]
             preprocessed_obs = obss_preprocessor(obs, device=device)
             with torch.no_grad():
-                if args.model_mem:
-                    # taking the memory till the length of time_step_inds, as demos beyond that have already finished
-                    _, _, new_memory = acmodel(preprocessed_obs, memory[:len(time_step_inds),:])    
+                # taking the memory till the length of time_step_inds, as demos beyond that have already finished
+                _, _, new_memory = acmodel(preprocessed_obs, memory[:len(inds),:])    
 
-            if args.model_mem:
-                for i in range(len(time_step_inds)):
-                    # Copying to the memories at the corresponding locations
-                    memories[time_step_inds[i],:] = memory[i,:]
-                memory[:len(time_step_inds),:] = new_memory
-            # Updating time_step_inds, by removing those indices corresponding to which the demonstrations have finished
-            time_step_inds = time_step_inds[:len(time_step_inds)-sum(done_step)]
-            if len(time_step_inds) == 0:
+            for i in range(len(inds)):
+                # Copying to the memories at the corresponding locations
+                memories[inds[i],:] = memory[i,:]
+            memory[:len(inds),:] = new_memory
+            
+            # Updating inds, by removing those indices corresponding to which the demonstrations have finished
+            inds = inds[:len(inds)-sum(done_step)]
+            if len(inds) == 0:
                 break
             # Incrementing the remaining indices
-            time_step_inds = [index+1 for index in time_step_inds]
-
+            inds = [index+1 for index in inds]
 
         # Here, actual backprop upto args.recurrence happens
-        while True:
-            memory = memories[inds,:]
-            final_loss = 0
-            # taking observations till that recurrence, and computing backprop on them 
-            for i in range(args.recurrence):
-                obs = obss[inds]
-                preprocessed_obs = obss_preprocessor(obs, device=device)
-                action_step = action_true[inds]
-                done_step = done[inds]
-                if args.model_mem:
-                    dist, value, memory = acmodel(preprocessed_obs, memory)
+        final_loss = 0
+        final_entropy, final_policy_loss, final_value_loss = 0,0,0
 
-                entropy = dist.entropy().mean()
-                policy_loss = -dist.log_prob(action_step).mean()
-                value_loss = (value - values[inds]).pow(2).mean()
-                loss = policy_loss - args.entropy_coef * entropy + args.value_loss_coef * value_loss
-                final_loss += loss
+        indexes = starting_indexes(num_frames)
+        memory = memories[indexes]
 
-                # Log some values
-                log["entropy"].append(float(entropy))
-                log["policy_loss"].append(float(policy_loss))
-                log["value_loss"].append(float(value_loss))
+        for _ in range(args.recurrence):
+            obs = obss[indexes]
+            preprocessed_obs = obss_preprocessor(obs, device=device)
+            action_step = action_true[indexes]
+            done_step = done[indexes]
+            mask_step = mask[indexes]
+            if args.model_mem:
+                dist, value, memory = acmodel(preprocessed_obs, memory*mask_step)
+            entropy = dist.entropy().mean()
+            policy_loss = -dist.log_prob(action_step).mean()
+            value_loss = (value - values[indexes]).pow(2).mean()
+            loss = policy_loss - args.entropy_coef * entropy + args.value_loss_coef * value_loss
+            final_loss += loss
+            final_entropy += entropy
+            final_policy_loss += policy_loss
+            final_value_loss += value_loss
+            indexes += 1
 
-                inds = inds[:len(inds)-sum(done_step)]
-                memory = memory[:len(inds)]
-                if len(inds) == 0:
-                    break
-                inds = [index+1 for index in inds]
+        final_loss /= args.recurrence
+        log["entropy"].append(float(final_entropy/args.recurrence))
+        log["policy_loss"].append(float(final_policy_loss/args.recurrence))
+        log["value_loss"].append(float(final_value_loss/args.recurrence))
 
-            final_loss /= args.recurrence
-            optimizer.zero_grad()
-            final_loss.backward()
-            optimizer.step()
+        optimizer.zero_grad()
+        final_loss.backward()
+        optimizer.step()
 
-            if len(inds) == 0:
-                break     
+                 
         offset += batch_size
     return log
+
+
+def validate():
+    env.seed(args.val_seed)
+    print("Validating the model")
+    args.deterministic = False
+    agent = utils.load_agent(args, env)
+    returnn = 0
+    agent.acmodel = acmodel
+    for _ in range(args.val_episodes):
+        obs = env.reset()
+        done = False
+        return_episode = 0
+        while not(done):
+            action = agent.get_action(obs)
+            obs, reward, done, _ = env.step(action)
+            agent.analyze_feedback(reward, done)
+            return_episode += reward
+        returnn += return_episode
+    returnn /= args.val_episodes
+    return returnn 
+
 
 total_start_time = time.time()
 
 demos,flat_demos = calculate_values()
 
-for i in range(1,args.epochs+1): 
+
+best_mean_return = 0
+patience = 0
+i = 0
+
+while True: 
+    i += 1
     update_start_time = time.time()   
     scheduler.step()
-    
     if args.model_mem:
         log = run_epoch_recurrence()
     else:
         log = run_epoch_norecur()
 
     update_end_time = time.time()
+
+    if i % args.validation_interval == 0:
+        if torch.cuda.is_available():
+            acmodel.cpu()
+        mean_return = validate()
+        print("Mean Return %.3f" % mean_return)
+        if mean_return > best_mean_return:
+            best_mean_return = mean_return
+            patience = 0
+            # Saving the model
+            print("Saving best model")
+            obss_preprocessor.vocab.save()
+            if torch.cuda.is_available():
+                acmodel.cpu()
+            utils.save_model(acmodel, model_name)
+            if torch.cuda.is_available():
+                acmodel.cuda()
+        else:
+            print("Losing Patience")
+            patience += 1
+            if patience > args.patience:
+                break
+            if torch.cuda.is_available():
+                acmodel.cuda()
 
     # Print logs
     total_ellapsed_time = int(time.time() - total_start_time)
@@ -339,13 +386,4 @@ for i in range(1,args.epochs+1):
         writer.add_scalar("duration", total_ellapsed_time, i)
         writer.add_scalar("entropy", log["entropy"], i)
         writer.add_scalar("policy_loss", log["policy_loss"], i)
-
-    if args.save_interval > 0 and i % args.save_interval == 0:
-        obss_preprocessor.vocab.save()
-        if torch.cuda.is_available():
-            acmodel.cpu()
-        utils.save_model(acmodel, model_name)
-        if torch.cuda.is_available():
-            acmodel.cuda()
-
 
