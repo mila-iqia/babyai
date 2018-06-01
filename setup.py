6bd0fafaa3ef74398f8d9e27e218a2866312a6e0
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-05-31 23:00:12 -0400

    Added dependency_links to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
     install_requires=[
         'gym>=0.9.6',
         'numpy>=1.10.0',
-        'pyqt5>=5.10.1'
+        'pyqt5>=5.10.1',
+        'gym_minigrid',
+        'torch_rl'
+    ],
+    dependency_links=[
+        'git+https://github.com/maximecb/gym-minigrid.git#egg=gym_minigrid-0',
+        'git+https://github.com/lcswillems/pytorch-a2c-ppo.git#egg=torch_rl-0'
     ]
 )
