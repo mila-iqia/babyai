cd8b1dd3871fa58735a0c8065db92cce216d41f1
saleml <salemlahlou9@gmail.com>
2018-06-07 18:42:14 -0400

    Changed the storage directory to be in the root folder, rather than the folder from which any script is run

--- babyai/utils/__init__.py
+++ babyai/utils/__init__.py
@@ -6,7 +6,11 @@ import torch
 def storage_dir():
     if "BABYAI_STORAGE" in os.environ:
         return os.environ["BABYAI_STORAGE"]
-    return "storage"
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    parent_directory = os.path.dirname(current_directory)
+    root_directory = os.path.dirname(parent_directory)
+    return os.path.join(root_directory, 'storage')
+
 
 def create_folders_if_necessary(path):
     dirname = os.path.dirname(path)
