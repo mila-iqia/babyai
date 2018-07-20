723d5b107b4e066ec10e82ec49cbe47905dcddee
Dzmitry Bahdanau <dimabgv@gmail.com>
2018-07-20 16:02:33 -0400

    script to print all instructions of a level (#101)
    * script to print all instructions of a level
    
    * delete trailing newlines

---
+++ scripts/show_level_instructions.py
@@ -0,0 +1,19 @@
+"""Randomly sample instructions from a level."""
+
+import argparse
+
+import babyai
+import gym
+
+
+parser = argparse.ArgumentParser("Show level instructions")
+parser.add_argument("--n-episodes", type=int, default=10000,
+                    help="Collect instructions from this many episodes")
+parser.add_argument("level",
+                    help="The level of interest")
+args = parser.parse_args()
+
+env = gym.make(args.level)
+instructions = set(env.reset()['mission'] for i in range(args.n_episodes))
+for instr in sorted(instructions):
+    print(instr)
