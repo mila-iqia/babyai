09b6b78429a1192017abffb74e00f58c124c8013
saleml <salemlahlou9@gmail.com>
2018-08-17 15:48:42 -0400

    fix bugs with evaluating demos

--- scripts/evaluate_all_demos.py
+++ scripts/evaluate_all_demos.py
@@ -1,3 +1,7 @@
+'''
+Script to evaluate all available demos. It assumes all demos (human and agent, except the "valid" ones)
+are generated with seed 1
+'''
 import os
 from subprocess import call
 import sys
@@ -8,7 +12,11 @@ folder = os.path.join(utils.storage_dir(), "demos")
 for filename in sorted(os.listdir(folder)):
     if filename.endswith(".pkl"):
         env = filename.split("_")[0]
-        demos_maker = filename.split("_")[-1].split('.')[0]
-        print("> Env: {} - {}".format(env, demos_maker))
-        command = ["python evaluate.py --env {} --demos-origin {}".format(env, demos_maker)] + sys.argv[1:]
+        demos = filename[:-4]  # Remove the .pkl part of the name
+        if not env.startswith('BabyAI-') and not env.endswith('-v0'):
+            print("> File: {} has to start with the level name (BabyAI-xxx-v0)".format(demos))
+            continue
+        print("> Env: {} - {}".format(env, demos))
+        seed = 0 if demos.endswith('valid') else 1
+        command = ["python evaluate.py --env {} --demos {} --seed {}".format(env, demos, seed)] + sys.argv[1:]
         call(" ".join(command), shell=True)
