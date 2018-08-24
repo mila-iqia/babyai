4ab9be21b48424199dd4636dc8945a0c5d086197
rizar <dimabv@tut.by>
2018-08-24 14:51:18 -0400

    use GPU for evaluation

--- babyai/evaluate.py
+++ babyai/evaluate.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+import gym
+
 # Returns the performance of the agent on the environment for a particular number of episodes.
 def evaluate(agent, env, episodes, model_agent=True, offsets=None):
     # Initialize logs
@@ -34,4 +38,66 @@ def evaluate(agent, env, episodes, model_agent=True, offsets=None):
         logs["return_per_episode"].append(returnn)
     if model_agent:
         agent.model.train()
+    return logs
+    
+
+class ManyEnvs(gym.Env):
+
+    def __init__(self, envs):
+        self.envs = envs
+        self.done = [False] * len(self.envs)
+
+    def reset(self):
+        many_obs = [env.reset() for env in self.envs]
+        self.done = [False] * len(self.envs)
+        return many_obs
+
+    def step(self, actions):
+        self.results = [env.step(action) #if not done else self.last_results[i]
+                        for i, (env, action, done)
+                        in enumerate(zip(self.envs, actions, self.done))]
+        self.done = [result[2] for result in self.results]
+        self.last_results = self.results
+        return zip(*self.results)
+
+    def render(self):
+        raise NotImplementedError
+
+
+# Returns the performance of the agent on the environment for a particular number of episodes.
+def batch_evaluate(agent, env_name, seed, episodes):
+    num_envs = 256
+
+    envs = []
+    for i in range(num_envs):
+        env = gym.make(env_name)
+        env.seed(100 * seed + i)
+        envs.append(env)
+    env = ManyEnvs(envs)
+
+    logs = {"num_frames_per_episode": [], 
+            "return_per_episode": [], 
+            "observations_per_episode": []}
+
+    for i in range((episodes + num_envs - 1) // num_envs):
+        many_obs = env.reset()
+
+        cur_num_frames = 0
+        num_frames = np.zeros((num_envs,), dtype='int64')
+        returns = np.zeros((num_envs,))
+        already_done = np.zeros((num_envs,), dtype='bool')
+        while (num_frames == 0).any():
+            action = agent.act_batch(many_obs)['action']
+            many_obs, reward, done, _ = env.step(action)
+            agent.analyze_feedback(reward, done)
+            done = np.array(done)
+            just_done = done & (~already_done)
+            returns += reward * just_done
+            cur_num_frames += 1
+            num_frames[just_done] = cur_num_frames
+            already_done[done] = True
+
+        logs["num_frames_per_episode"].extend(list(num_frames))
+        logs["return_per_episode"].extend(list(returns))
+
     return logs
