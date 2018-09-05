5882ef28700fd590814552dc4384caff1658df96
saleml <salemlahlou9@gmail.com>
2018-09-05 16:52:42 -0400

    evaluate all models in a folder on a specific env without them being necessarily in the format BabyAI-....

--- scripts/evaluate_all_models.py
+++ scripts/evaluate_all_models.py
@@ -8,13 +8,15 @@ import babyai.utils as utils
 parser = argparse.ArgumentParser()
 parser.add_argument("--episodes", type=int, default=1000,
                     help="number of episodes of evaluation (default: 1000)")
+parser.add_argument("--env", required=False,
+                    help="if specified, all models will be evaluated on this level")
 args = parser.parse_args()
 
 folder = os.path.join(utils.storage_dir(), "models")
 for model in sorted(os.listdir(folder)):
     if model.startswith('.'):
         continue
-    env = model.split("_")[0]
-    print("> Env: {}".format(env))
+    env = args.env or model.split("_")[0]
+    print("> Env: {} > Model: {}".format(env, model))
     command = ["python evaluate.py --env {} --model {} --episodes {}".format(env, model, args.episodes)] + sys.argv[1:]
     call(" ".join(command), shell=True)
