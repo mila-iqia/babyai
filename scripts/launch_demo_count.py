85f1391d19c998991dedb0b5fc5f5f6fde27aad2
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-09-18 12:58:35 -0400

    Added test modes for launch scripts

--- scripts/launch_demo_count.py
+++ scripts/launch_demo_count.py
@@ -11,12 +11,14 @@ import argparse
 import math
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--test", action='store_true', help="print launch commands without executing them")
 parser.add_argument("--env", required=True, help="name of the environment to train on (REQUIRED)")
 parser.add_argument("--demos", required=True, help="demos filename (REQUIRED or demos-origin required)")
 parser.add_argument("--seed", type=int, default=0, help="random seed to use")
 parser.add_argument("--min-demos", type=int, default=30000, help="minimum number of demos to test")
 parser.add_argument("--max-demos", type=int, default=1000000, help="maximum number of demos to test")
 parser.add_argument("--step-size", type=float, default=math.sqrt(2), help="step size for the demo counts")
+args = parser.parse_args()
 
 SBATCH_PARAMS = '--account=rpp-bengioy --mail-user maximechevalierb@gmail.com --mail-type ALL --ntasks=1'
 MODEL_PARAMS = '--memory-dim=2048 --recurrence=80 --batch-size=128 --instr-arch=attgru --instr-dim=256'
@@ -25,11 +27,12 @@ def launch_job(launch_cmd, job_name):
     log_name = 'slurm-{}-%j.out'.format(job_name)
     sbatch_cmd = 'sbatch {} --time=72:00:00 --gres=gpu:1 -c 4 --mem=16000M --error={} --output={} scripts/sbatch/run.sh {}'.format(SBATCH_PARAMS, log_name, log_name, launch_cmd)
     print('launching job:', sbatch_cmd)
-    subprocess.check_call(sbatch_cmd, shell=True)
-    print()
-    time.sleep(1)
 
-args = parser.parse_args()
+    if not args.test:
+        subprocess.check_call(sbatch_cmd, shell=True)
+        time.sleep(1)
+
+    print()
 
 demo_counts = []
 demo_count = args.max_demos
--- scripts/launch_max_experiments.py
+++ scripts/launch_max_experiments.py
@@ -3,6 +3,11 @@
 import os
 import time
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", action='store_true', help="print launch commands without executing them")
+args = parser.parse_args()
 
 SBATCH_PARAMS = '--account=rpp-bengioy --mail-user maximechevalierb@gmail.com --mail-type ALL --ntasks=1'
 #MODEL_PARAMS = '--memory-dim=2048 --recurrence=80 --batch-size=128 --instr-dim=256'
@@ -12,9 +17,12 @@ def launch_job(launch_cmd, job_name, time_limit):
     log_name = 'slurm-{}-%j.out'.format(job_name)
     sbatch_cmd = 'sbatch {} --time={} --gres=gpu:1 -c 4 --mem=16000M --error={} --output={} scripts/sbatch/run.sh {}'.format(SBATCH_PARAMS, time_limit, log_name, log_name, launch_cmd)
     print('launching job:', sbatch_cmd)
-    subprocess.check_call(sbatch_cmd, shell=True)
+
+    if not args.test:
+        subprocess.check_call(sbatch_cmd, shell=True)
+        time.sleep(1)
+
     print()
-    time.sleep(1)
 
 def launch_intelligent_expert(seed, level, time_limit):
     env = 'BabyAI-{}-v0'.format(level)
