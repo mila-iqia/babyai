45fa9ca1539b41959b0d4849f11b2a78241fa3fb
Dzmitry Bahdanau <dimabgv@gmail.com>
2018-10-12 12:17:34 -0400

    edit README (#206)

--- scripts/gui.py
+++ scripts/gui.py
@@ -371,7 +371,7 @@ def main(argv):
     parser.add_option(
         "--env-name",
         help="gym environment to load",
-        default='MiniGrid-MultiRoom-N6-v0'
+        default='BabyAI-BossLevel-v0'
     )
     (options, args) = parser.parse_args()
 
