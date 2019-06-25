8e597a0af48ffbf4b5e597c7adbda60a5b5cdae3
rizar <dimabv@tut.by>
2019-06-25 17:29:39 +0000

    fix demos name, val_episodes as keyword argument

--- babyai/efficiency.py
+++ babyai/efficiency.py
@@ -14,10 +14,10 @@ from babyai.cluster_specific import launch_job
 BIG_MODEL_PARAMS = '--memory-dim=2048 --recurrence=80 --batch-size=128 --instr-arch=attgru --instr-dim=256'
 SMALL_MODEL_PARAMS = '--batch-size=256'
 
-def main(env, seed, min_demos, max_demos=None,
-         step_size=math.sqrt(2), pretrained_model=None):
-    level_size = 'big' if env == 'BabyAI-GoTo-v0' else 'small'
-    demos = env + "-seed1"
+def main(env, seed, n_examples, min_demos, max_demos=None,
+         step_size=math.sqrt(2), pretrained_model=None, level_type='small',
+         val_episodes=512):
+    demos = env
 
     if not max_demos:
         max_demos = min_demos
@@ -31,11 +31,8 @@ def main(env, seed, min_demos, max_demos=None,
 
     for demo_count in demo_counts:
         # Decide on the parameters
-        epoch_length = 25600
-        if level_size == 'big':
-            epoch_length = 51200
-        target_examples = 1000000 * (40 if level_size == 'big' else 80)
-        epochs = target_examples // epoch_length
+        epoch_length = 25600 if level_type == 'small' else 51200
+        epochs = n_examples // epoch_length
 
         # Print info
         print('{} demos, {} epochs of {} examples'.format(demo_count, epochs, epoch_length))
@@ -45,11 +42,11 @@ def main(env, seed, min_demos, max_demos=None,
         if pretrained_model:
             model_name += '_{}'.format(pretrained_model)
         jobname = '{}_efficiency'.format(demos, min_demos, max_demos)
-        model_params = BIG_MODEL_PARAMS if level_size == 'big' else SMALL_MODEL_PARAMS
-        cmd = ('{model_params} '
+        model_params = BIG_MODEL_PARAMS if level_type == 'big' else SMALL_MODEL_PARAMS
+        cmd = ('{model_params} --val-episodes {val_episodes}'
                ' --seed {seed} --env {env} --demos {demos}'
                ' --val-interval 1 --log-interval 1 --epoch-length {epoch_length}'
-               ' --model {model_name} --episodes {demo_count} --epochs {epochs}'
+               ' --model {model_name} --episodes {demo_count} --epochs {epochs} --patience {epochs}'
           .format(**locals()))
         if pretrained_model:
             cmd += ' --pretrained-model {}'.format(pretrained_model)
