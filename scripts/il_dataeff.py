fc9c6f1d970b16881fede0f7c98bb62a896aef63
rizar <dimabv@tut.by>
2019-09-28 23:19:02 +0000

    generate summaries

--- scripts/il_dataeff.py
+++ scripts/il_dataeff.py
@@ -12,9 +12,11 @@ parser.add_argument("--regex", default='.*')
 parser.add_argument("--patience", default=2, type=int)
 parser.add_argument("--window", default=1, type=int)
 parser.add_argument("--limit", default="frames")
+parser.add_argument("--summary_path", default='summary.csv')
 args = parser.parse_args()
 
 df = pandas.concat(plotting.load_logs(args.path), sort=True)
 plotting.min_num_samples(
     df, args.regex,
-    patience=args.patience, window=args.window, limit=args.limit)
+    patience=args.patience, window=args.window, limit=args.limit,
+    summary_path=args.summary_path)
