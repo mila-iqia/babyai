1c54e115f195cce818a050e5a849488be29f63b4
rizar <dimabv@tut.by>
2019-09-29 00:34:47 +0000

    sample efficiency estimation with GP

--- babyai/plotting.py
+++ babyai/plotting.py
@@ -24,6 +24,11 @@ import re
 import numpy as np
 from matplotlib import pyplot
 import pandas
+import scipy
+from scipy import stats
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+from scipy.linalg import cholesky, cho_solve, solve_triangular
 
 
 def load_log(dir_):
@@ -184,9 +189,102 @@ def min_num_samples(df, regex, patience, limit='epochs', window=1, normal_time=N
     summary_df = pandas.DataFrame(summary_data, columns=('num_samples', 'success_rate'))
     summary_df.to_csv(summary_path)
 
-    print("min samples required is {}".format(min_samples_required))
     if min(num_samples) == min_samples_required:
         print('should be run with less samples!')
     if need_more_time:
         print('should be run for more time!')
-    return min_samples_required, normal_time
+    return summary_df
+
+
+def estimate_sample_efficiency(df, visualize=False, figure_path=None):
+    f, axes = pyplot.subplots(1, 2, figsize=(15, 5))
+
+    print("{} datapoints".format(len(df)))
+    x = np.log2(df['num_samples'].values)
+    y = (df['success_rate'] - 0.99).values * 100
+    print("min x: {}, max x: {}, min y: {}, max y: {}".format(x.min(), x.max(), y.min(), y.max()))
+
+    if (y < 0).sum() < 5:
+        print("ATTENTION: you have less than 5 datapoints below the threshold.")
+        print("Consider running experiments with less data.")
+    if (y > 0).sum() < 5:
+        print("ATTENTION: you have less than 5 datapoints above the threshold.")
+        print("Consider running experiments with more data.")
+
+    kernel = 1.0 * RBF() + WhiteKernel(noise_level_bounds=(1e-10, 1))
+    gp = GaussianProcessRegressor(kernel=kernel, alpha=0, normalize_y=False).fit(x[:, None], y)
+    print("Kernel:", gp.kernel_)
+    print("Marginal likelihood:", gp.log_marginal_likelihood_value_)
+
+    grid = np.arange(x[0], x[-1], 0.02)
+    y_grid_mean, y_grid_cov = gp.predict(grid[:, None], return_cov=True)
+    noise_level = gp.kernel_.k2.noise_level
+    f_grid_cov = y_grid_cov - np.diag(np.ones_like(y_grid_cov[0]) * noise_level)
+
+    if visualize:
+        axis = axes[0]
+        axis.plot(x, y, 'o')
+        axis.plot(grid, y_grid_mean)
+        axis.set_xlabel('log2(N)')
+        axis.set_ylabel('accuracy minus 99%')
+        axis.set_title('Data Points & Posterior')
+        axis.fill_between(grid, y_grid_mean - np.sqrt(np.diag(y_grid_cov)),
+                         y_grid_mean + np.sqrt(np.diag(y_grid_cov)),
+                         alpha=0.2, color='k')
+        axis.fill_between(grid, y_grid_mean -np.sqrt(np.diag(f_grid_cov)),
+                 y_grid_mean + np.sqrt(np.diag(f_grid_cov)),
+                 alpha=0.2, color='g')
+        axis.hlines(0, x[0], x[-1])
+
+    probs = []
+    total_p = 0.
+    print("Estimating N_min using a grid of {} points".format(len(grid)))
+    for j in range(len(grid)):
+        if j and j % 10 == 0:
+            print('{} points done'.format(j))
+            print(" ".join(["{:.3g}".format(p) for p in probs[-10:]]))
+        mu = y_grid_mean[:j + 1].copy()
+        mu[j] *= -1
+        sigma = f_grid_cov[:j + 1, :j + 1].copy()
+        sigma[j, :j] *= -1
+        sigma[:j, j] *= -1
+        sigma[np.diag_indices_from(sigma)] += 1e-6
+        p = stats.multivariate_normal.cdf(np.zeros_like(mu), mu, sigma, abseps=1e-3, releps=1e-3)
+        probs.append(p)
+        total_p += p
+        if total_p.sum() > 0.999:
+            print('the rest is unlikely')
+            break
+    probs = np.array(probs)
+    if (probs.sum() - 1) > 0.01:
+        raise ValueError("oops, probabilities don't sum to one")
+    else:
+        # probs should sum to 1, but there is always a bit of error
+        probs = probs / probs.sum()
+
+    cut_grid = grid[:len(probs)]
+    mean_n_min = (probs * cut_grid).sum()
+    mean_n_min_squared = (probs * cut_grid ** 2).sum()
+    print(mean_n_min_squared, mean_n_min ** 2)
+    std_n_min = (mean_n_min_squared - mean_n_min ** 2) ** 0.5
+    print('N_min mean and std: ({}, {})'.format(mean_n_min, std_n_min))
+
+    left, right = (mean_n_min - 3 * std_n_min, mean_n_min + 3 * std_n_min)
+    print("Confidence interval (log):", left, mean_n_min, right)
+    print("Confidence interval:", 2 ** left, 2 ** mean_n_min, 2 ** right)
+
+    if visualize:
+        axis = axes[1]
+        axis.plot(x, y, 'o')
+        axis.set_xlabel('log2(N)')
+        axis.set_ylabel('accuracy minus 99%')
+        axis.hlines(0, x[0], x[-1])
+        axis.vlines(left, min(y), max(y), color='r')
+        axis.vlines(mean_n_min, min(y), max(y), color='k')
+        axis.vlines(right, min(y), max(y), color='r')
+        axis.set_title('Data points & Conf. interval for min. number of samples')
+
+    pyplot.tight_layout()
+    if figure_path:
+        pyplot.savefig(figure_path)
+    return mean_n_min, std_n_min, probs
--- scripts/il_dataeff.py
+++ scripts/il_dataeff.py
@@ -13,10 +13,12 @@ parser.add_argument("--patience", default=2, type=int)
 parser.add_argument("--window", default=1, type=int)
 parser.add_argument("--limit", default="frames")
 parser.add_argument("--summary_path", default='summary.csv')
+parser.add_argument("--figure_path", default='figure.png')
 args = parser.parse_args()
 
-df = pandas.concat(plotting.load_logs(args.path), sort=True)
-plotting.min_num_samples(
-    df, args.regex,
+df_logs = pandas.concat(plotting.load_logs(args.path), sort=True)
+df_success_rate = plotting.min_num_samples(
+    df_logs, args.regex,
     patience=args.patience, window=args.window, limit=args.limit,
     summary_path=args.summary_path)
+plotting.estimate_sample_efficiency(df_success_rate, visualize=True, figure_path=args.figure_path)
