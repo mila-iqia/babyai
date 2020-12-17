3b6b3c5356c481a649f5df64bad409117fec86ac
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2020-12-17 00:44:22 -0500

    Fix issue #98, enjoy.py needs to be updated for new gym-minigrid

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='babyai',
-    version='1.1',
+    version='1.1.2',
     license='BSD 3-clause',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     packages=['babyai', 'babyai.levels', 'babyai.utils'],
     install_requires=[
         'gym>=0.9.6',
-        'numpy==1.15.4', # Temporary: fix numpy version because of bug introduced in 1.16
+        'numpy>=1.17.0',
         "torch>=0.4.1",
         'blosc>=1.5.1',
         'gym_minigrid @ https://github.com/maximecb/gym-minigrid/archive/master.zip'
