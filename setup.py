898b1ba8c14ba7ba316d784a4a9770ed26cbf489
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2020-12-17 00:12:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['babyai', 'babyai.levels', 'babyai.utils'],
     install_requires=[
         'gym>=0.9.6',
-        'numpy==1.15.4', # Temporary: fix numpy version because of bug introduced in 1.16
+        'numpy>=1.17.0',
         "torch>=0.4.1",
         'blosc>=1.5.1',
         'gym_minigrid @ https://github.com/maximecb/gym-minigrid/archive/master.zip'
