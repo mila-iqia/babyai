e8e360c9a8f828ce35c7bee41465d1aaa995efdf
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-03-02 11:16:23 -0500

    updated notice

--- babyai/__init__.py
+++ babyai/__init__.py
@@ -4,12 +4,13 @@ from . import levels
 from . import utils
 import warnings
 
+
 warnings.warn(
-    "This code base is no longer maintained, and is not expected to be maintained again in the future. \n"
-    "These environments been maintained inside of Minigrid"
+    "This code base is no longer maintained and is not expected to be maintained again. \n"
+    "These environments are now maintained within Minigrid"
     "(see https://github.com/Farama-Foundation/Minigrid/tree/master/minigrid/envs/babyai). \n"
-    "This maintained version includes documentation, support for current versions of Python,\n"
-    "numerous bug fixes, support for installation via pip, and numerous other large quality of life improvements.\n"
-    "We encourage researchers to switch to this maintained version for all purposes other than comparing\n"
-    "to results run on this version of the environments. \n"
+    "The maintained version includes documentation, support for current versions of Python, \n"
+    "numerous bug fixes, support for installation via pip, and many other quality-of-life improvements. \n"
+    "We encourage researchers to switch to the maintained version for all purposes other than comparing \n"
+    "with results that use this version of the environments. \n"
 )
